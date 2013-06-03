5463a9ac75e41035fa80a09e7a48c5a9562d5619
Pierre Thiery <pierre@nothos.net>
2013-06-03 16:16:46 +0200

    Add RDF eq?

--- turtle.lisp
+++ turtle.lisp
@@ -9,14 +9,27 @@
 ; RDF graph components
 
 (defclass resource ()
-  ((uri :initarg :uri)))
+  ((uri :initarg :uri :reader uri)))
 
 (defclass qnamed-resource (resource)
   ((qual :initarg :qual)
    (name :initarg :name)))
 
 (defclass blank ()
-  ((id :initarg :id)))
+  ((id :initarg :id :reader id)))
+
+
+(defgeneric rdf-eq? (subj1 subj2))
+
+(defmethod rdf-eq? ((subj1 resource) (subj2 resource))
+  (equal (uri subj1) (uri subj2)))
+
+(defmethod rdf-eq? ((subj1 blank) (subj2 blank))
+  (equal (id subj1) (id subj2)))
+
+(defmethod rdf-eq? ((subj1 resource) (subj2 blank)))
+(defmethod rdf-eq? ((subj1 blank) subj2)
+  (rdf-eq? subj2 subj1))
 
 
 ; turtle commands
