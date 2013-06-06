2c5bf3ad3b117d19a4f5acbb5b98b6cfe1d612c4
Pierre Thiery <pierre@nothos.net>
2013-06-06 15:48:33 +0200

    Adding AGPL license

--- turtle.lisp
+++ turtle.lisp
@@ -1,3 +1,19 @@
+ #| Aquila - A prototype converter from Emdros to RDF
+    Copyright (C) 2013 Pierre Thierry <pierre@nothos.net>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. |#
+
 (in-package :nothos.net/2013.05.aquila)
 
 (defclass turtle-doc ()
